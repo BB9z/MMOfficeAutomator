6b301f2ffc7c0a13891a3e9c4a3f48cc19aa52f0
BB9z <bb9z@me.com>
2019-12-01 20:23:36 +0800

    Config, log test.

--- Scripts/main.ps1
+++ Scripts/main.ps1
@@ -1,1 +1,18 @@
-Get-Location
+$config = Get-Content -Path .\config.json -Raw | ConvertFrom-Json
+
+if ($config.debug) {
+    $DebugPreference = "Continue"
+}
+else {
+    $DebugPreference = "SilentlyContinue"
+}
+
+. "$PSScriptRoot/Log.ps1"
+
+foreach ($item in $config.organization_list) {
+    Out-Info $item
+}
+
+# $env:PSModulePaths
+# New-Object System.IO.FileSystemWatchers
+
